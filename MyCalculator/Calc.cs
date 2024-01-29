c10e89dcd35a01eb3e27921e3c6f2e5f35b9730d
abhishekb13402 <abhishekbhatt13402@gmail.com>
2024-01-29 18:20:42 +0530

    update code, dll file

---
+++ MyCalculator/Calc.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCalculator
+{
+    public class Calc
+    {
+        /// <summary>
+        /// This method is used for adding two numbers.
+        /// </summary>
+        /// <param name="num1">Pass number 1 value.</param>
+        /// <param name="num2">Pass number 2 value</param>
+        /// <returns></returns>
+        public int Sum(int num1, int num2)
+        {
+            return num1 + num2; 
+        }
+
+        public int Substract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public int Multiplication (int num1, int num2) 
+        {
+            return num1 * num2;
+        }
+
+        private int Division (int num1, int num2) 
+        {
+            return num1 / num2;
+        }
+    }
+}
